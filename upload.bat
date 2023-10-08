1dbc79d3c80dedb338dee28e160974f5bc814f87
PosetMage <PosetMage@gmail.com>
2023-10-08 12:39:16 +0800

    all

---
+++ upload.bat
@@ -0,0 +1,8 @@
+git config --local user.name "PosetMage"
+git config --local user.email "PosetMage@gmail.com"
+git remote set-url origin git@POM:posetmage/ar_test.git
+
+git pull
+git add .
+git commit -m "all"
+git push
